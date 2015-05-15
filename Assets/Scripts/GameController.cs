2f2d98d27a3b978bcab9b33d91763d3be5e5e89c
Jeff Lewison <jeffreylewison@gmail.com>
2015-05-14 19:54:37 -0500

    Zombie pumpkin slayer

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,197 @@
+﻿using Prime31.MetroEssentials;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject _pumpkin;
+    private int _score;
+    //how many pumpkins per wave?
+    [SerializeField]
+    private int _waveCount = 8;
+    //warmup time before spawns start
+    [SerializeField]
+    private float _startWait;
+    //30 seconds before the next wave
+    [SerializeField]
+    private float _waveWait = 30;
+
+    //Where's our cross to attack.
+    private Transform _tombstonePosition;
+
+    [SerializeField]
+    private GameObject[] _spawnPoints;
+
+    [SerializeField]
+    private Text _scoreText;
+
+    private int _crossHealth = InitialHealth;
+
+    [SerializeField]
+    private RectTransform _scoreMask;
+
+    [SerializeField]
+    private Color[] _hitColors;
+
+    ///We blink the cross red on damage, need its renderer component
+    private Renderer _crossRenderer;
+    /// <summary>
+    /// For sizing out the health bar.
+    /// </summary>
+    private RectTransform _healthMaskTransform;
+
+    private float _initialHealthMaskWidth;
+    private const int InitialHealth = 7;
+    private float _healthMaskHeight;
+    
+    /// <summary>
+    /// The game object containing all resurrect dialog stuff to show.
+    /// </summary>
+    [SerializeField]
+    private GameObject _resurrectDialog;
+    void Start()
+    {
+#if UNITY_METRO
+        SettingsPane.registerSettingsCommand("Privacy Policy", "This is my privacy policy. Consider a url as well to bring you to the site.");
+        
+        //Set the live tile. You can set a source as an internet url as well.
+        //This format simply means 'within the windows store application, look at the assets folder'
+        //You'll want to make sure this image appears in the generated visual studio project, as it won't 
+        //get pushed from Unity in any way. 
+        var images = new string[] { "ms-appx:///assets/310x150_attacking.png" };
+        Tiles.updateTile(TileTemplateType.TileWideImage, null, images); 
+#endif
+
+        //Find the cross
+        _tombstonePosition = GameObject.FindGameObjectWithTag("Cross").transform;
+        _healthMaskTransform = GameObject.Find("HealthMask").GetComponent<RectTransform>();
+        
+        //Since inactive gameobjects aren't searched (except by the FindObjectsOfTypeAll hack/deprecation)
+        if (!_resurrectDialog)
+        {
+            Debug.LogError("Assign the ResurrectDialog dialog reference through the editor.");
+        }
+        //Store the height/width of the mask. We'll move the mask back to adjust health later.
+        _initialHealthMaskWidth = _healthMaskTransform.sizeDelta.x;
+        _healthMaskHeight = _healthMaskTransform.sizeDelta.y;
+
+        _crossRenderer = _tombstonePosition.transform.Find("polySurface1").gameObject.GetComponent<Renderer>();
+        var trashMan = GameObject.Find("TrashMan");
+        if (!trashMan)
+        {
+            Debug.LogError("A trashman component wasn't found (an object pool) in this scene. Create one or comment this code out and use the SpawnWithoutPooling below instead of SpawnWitPooling");
+        }
+
+        //Kick off the routine to keep generating waves of pumpkins
+        StartCoroutine(SpawnWaves());
+        
+    }
+
+    public void ShowResurrectPurchase()
+    {
+        _resurrectDialog.SetActive(true);
+    }
+
+    public void HideResurrectPurchase()
+    {
+        _resurrectDialog.SetActive(false);
+    }
+
+    public void IncreaseScore()
+    {
+        _score++;
+        _scoreText.text = _score.ToString();
+    }
+    
+    IEnumerator SpawnWaves()
+    {
+        return SpawnWithPooling();
+        //return SpawnWithoutPooling();
+    }
+
+    IEnumerator SpawnWithPooling()
+    {
+        yield return new WaitForSeconds(_startWait);
+        for (int i = 0; i < _waveCount; i++)
+        {
+            //ask the object pool for an available pumpkin. We've limited to 8
+            //active at once to help avoid overload of too many tris in our scene.
+            var pumpkin = TrashMan.spawn("Pumpkin", _spawnPoints[Random.Range(0, 2)].transform.position);
+
+            //pumpkin may be null if one isn't available from the pool
+            if (pumpkin)
+            {
+                //Reset its properties
+                pumpkin.GetComponent<PumpkinController>().Reset();
+                //Look at the cross (so we move towards it)
+                pumpkin.transform.LookAt(_tombstonePosition);
+            }
+            else
+            {
+                Debug.Log(string.Format("A pumpkin was requested from the pool but not available, " +
+                                        "potentially a higher wavecount ({0}) than allowed in the pool?", 
+                                        _waveCount));
+            }
+
+            //Sleep
+            yield return new WaitForSeconds(Random.Range(.5f, 2.5f));
+        }
+        yield return new WaitForSeconds(_waveWait);
+    }
+
+    IEnumerator SpawnWithoutPooling()
+    {
+        yield return new WaitForSeconds(_startWait);
+        while (true)
+        {
+            for (int i = 0; i < _waveCount; i++)
+            {
+                //Choos a random spawn point
+                var spawnPosition = _spawnPoints[Random.Range(0, 2)].transform.position;
+
+                //Create the pumpkin
+                Quaternion spawnRotation = Quaternion.identity;
+                var newPumpkin = (GameObject)Instantiate(_pumpkin, spawnPosition, spawnRotation);
+
+                //Look at the cross (so we move towards it)
+                newPumpkin.transform.LookAt(_tombstonePosition);
+
+                //Pause for .5 - 2.5 seconds
+                yield return new WaitForSeconds(Random.Range(.5f, 2.5f));
+            }
+            yield return new WaitForSeconds(_waveWait);
+        }
+    }
+
+    public void DecrementCrossHealth()
+    {
+        Debug.Log("Cross health decremented, triggered from animation event");
+        if (_crossHealth <= 0) return;
+        _crossHealth--;
+        if (_crossHealth == 0)
+        {
+            ShowResurrectPurchase();
+        }
+        StartCoroutine(CoApplyCrossHit());
+        //Apply mask to health bar to make it smaller. This will in turn show less of the health bar.
+        //The mask simply covers the underlying gameobject. Where the mask is, it shows through to the health bar.
+        //If the mask is smaller, we see less of the health bar.
+        
+        //We are essentally multiplying the width by the current % of health. IF we have 75% health left,
+        //we are shrinking the mask to 75% of its initial width (and keeping height the same)
+       // _healthMaskTransform.sizeDelta = new Vector2(_initialHealthMaskWidth * (_crossHealth /  (float)InitialHealth), _healthMaskHeight);
+    }
+
+    private IEnumerator CoApplyCrossHit()
+    {
+
+        //Swap out the color for a damage color
+        _crossRenderer.material.color = _hitColors[0];
+
+        yield return new WaitForSeconds(.2f);
+        _crossRenderer.material.color = _hitColors[1];
+    }
+
+
+}
---
+++ Assets/Scripts/PumpkinController.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PumpkinController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 0f;
+    [SerializeField]
+    private Color[] _hitColors;
+
+    private const int InitialHealth = 4;
+
+    private int _health = InitialHealth;
+    private Renderer _renderer;
+    private Animator _animator;
+    private Vector3 _forward;
+    private Transform _particleTransform;
+    
+    //Keeps track if we're in attack mode when in range of target (cross)
+    private bool _inAttackRangeOfCross = false;
+
+    [SerializeField]
+    private GameObject _particleSystem;
+
+    private static GameController _gameController;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Find the grave.
+        _animator = GetComponentInChildren<Animator>();
+        _renderer = GetComponentInChildren<Renderer>();
+        _forward = Vector3.forward * _speed;
+        if (!_gameController)
+        {
+            _gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        }
+        if (!_gameController)
+        {
+            Debug.LogError("Could not find the GameController");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Move pumpkin towards cross
+        rigidbody.velocity = transform.TransformDirection(_forward);
+    }
+
+    private IEnumerator Die()
+    {
+        //Instantiate particles
+        Instantiate(_particleSystem, this.transform.position, Quaternion.identity);
+        gameObject.SetActive(false);
+        TrashMan.despawn(gameObject);
+        _gameController.IncreaseScore();
+        yield break;
+    }
+
+    private IEnumerator CoApplyDamage()
+    {
+
+        //Swap out the color for a damage color
+        _renderer.material.color = _hitColors[0];
+        yield return new WaitForSeconds(.2f);
+        _renderer.material.color = _hitColors[1];
+    }
+
+    public void ApplyDamage()
+    {
+        if (_health <= 0) return;
+
+        _health--;
+        if (_health <= 0)
+        {
+            StartCoroutine(Die());
+            return;
+        }
+        StartCoroutine(CoApplyDamage());
+    }
+
+    public void AttackCross()
+    {
+        StartCoroutine(CoAttackCross());
+    }
+
+    public void DecrementCrossHealth()
+    {
+        _gameController.DecrementCrossHealth();
+    }
+
+    private IEnumerator CoAttackCross()
+    {
+
+        //We loop until I'm dead or out of range
+        while (_inAttackRangeOfCross)
+        {   
+            //make the pumpkin bite
+            _animator.SetTrigger("Bite");
+            _gameController.DecrementCrossHealth();
+            yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+        }
+        
+    }
+
+    /// <summary>
+    /// Reset the values of this pumpkin to its default state ready for reuse
+    /// </summary>
+    public void Reset()
+    {
+        if (_animator)
+        {
+            //If animator has been initialized, reset it. First time pool loads it may not be available.
+            _animator.enabled = false;
+            _animator.enabled = true;
+        }
+        //When we die, we're turned inactive.
+        gameObject.SetActive(true);
+        _health = InitialHealth;
+        StopAllCoroutines();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Pumpkin collided with " + collision.gameObject.name);
+        var go = collision.gameObject;
+        if (go.tag == "CrossCenter")
+        {
+            //Get the pumpkins controller and tell it to act.
+            _inAttackRangeOfCross = true;
+            AttackCross();
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        
+        Debug.Log("Pumpkin collided with " + collision.gameObject.name);
+        var go = collision.gameObject;
+        if (go.tag == "CrossCenter")
+        {
+			//stop attacking the cross.
+            _inAttackRangeOfCross = false;
+        }
+    }
+
+
+}
